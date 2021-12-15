1c4f9e2922b988a3444e42667cbdaf7c7f125762
Valerie RAMAROZ <olv.ramaroz@gmail.com>
2021-12-15 19:38:33 +0100

    Design Frontend Login Signup OK

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img id="logo" alt="Groupomania logo" src="./assets/logo.png">
+  <p id="intro">Partagez et restez en contact avec vos collègues.</p>
+  <router-view></router-view>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;800&display=swap');
+
+
+* {
+  font-family: 'Poppins', sans-serif;
+  margin:0;
+  padding: 0;
+  box-sizing: border-box;
 }
+#app {
+  max-width: 100%;
+}
+body {
+  background-color: #F0F2F5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  padding:32px;
+}
+#logo {
+  width: 100%;
+  border-radius: 8px;
+}
+#intro {
+  text-align: center;
+  font-size: 1.2rem;
+  margin-bottom: 2rem;
+}
+.card {
+  max-width: 100%;
+  width: 540px;
+  background: #fff;
+  border-radius: 16px;
+  padding:32px;
+}
+.card_title {
+  text-align:center;
+  font-weight: 800;
+}
+.card_sub {
+  text-align: center;
+  color:#666;
+  font-weight: 500;
+}
+  .button {
+    background: #FD2D01;
+    color: #fff;
+    border-radius: 8px;
+    font-weight: 500;
+    font-size: 1rem;
+    border: none;
+    width: 100%;
+    padding: .8rem;
+    transition: .4s background-color;
+  }
+  .card_action {
+    color: #FD2D01;
+    text-decoration: underline;
+  }
+  .card_action:hover {
+    cursor:pointer;
+  }
+  .button:hover {
+    cursor: pointer;
+    background: #FFD7D7;
+    color: #000;
+  }
 </style>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,73 @@
+// Les VIEWS représentent les différentes pages qu'on a sur le site 
+
+<template>
+  <div class="card">
+    <h1 class="card_title" v-if="mode == 'login'">Se connecter</h1>
+    <h1 class="card_title" v-else>Créer un compte</h1>
+    <p class="card_sub" v-if="mode == 'login'"> Pas de compte ? Inscris-toi 😉 <span class="card_action" @click="switchToCreateAccount()">Créer un compte</span></p>
+    <p class="card_sub" v-else>Tu as déjà créé ton profil ? 🥰 <span class="card_action" @click="switchToLogin()">Se connecter</span></p>
+    <div class="form-row">
+      <input v-model="email" class="form-row_input" type="text" placeholder="Adresse email"/>
+    </div>
+    <div class="form-row" v-if="mode == 'create'">
+      <input v-model="nom" class="form-row_input" type="text" placeholder="Nom"/>
+      <input v-model="prenom" class="form-row_input" type="text" placeholder="Prénom"/>
+    </div>
+    <div class="form-row">
+      <input v-model="password" class="form-row_input" type="password" placeholder="Mot de passe"/>
+    </div>
+    <div class="form-row">
+      <button class="button" v-if="mode == 'login'">Connexion</button>
+      <button class="button" v-else>Inscription</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            mode: "login",
+            email: "",
+            password: "",
+            nom: "",
+            prenom: "",
+        }
+    },
+        methods: {
+        switchToCreateAccount() {
+            this.mode = "create";
+        },
+        switchToLogin() {
+            this.mode = "login";
+        },
+    }
+
+}
+
+</script>
+
+
+<style>
+    .form-row {
+    display: flex;
+    margin: 16px 0px;
+    gap:16px;
+    flex-wrap: wrap;
+  }
+  .form-row_input {
+    padding:8px;
+    border: none;
+    border-radius: 8px;
+    background:#f2f2f2;
+    font-weight: 500;
+    font-size: 16px;
+    flex:1;
+    min-width: 100px;
+    color: black;
+  }
+  .form-row_input::placeholder {
+    color:#202020;
+  }
+
+</style>
